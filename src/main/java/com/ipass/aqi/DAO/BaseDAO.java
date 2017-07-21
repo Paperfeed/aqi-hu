d888a1210833df58f7e995dccf121736f55bd1a1
Boris <adminboris@gmail.com>
2017-07-21 23:42:57 +0200

    final edit

--- src/main/java/com/ipass/aqi/DAO/BaseDAO.java
+++ src/main/java/com/ipass/aqi/DAO/BaseDAO.java
@@ -19,8 +19,8 @@ public class BaseDAO {
 			Properties props = new Properties();
 			props.put(Context.SECURITY_PRINCIPAL, "hngiujnqgfyzhs");
 			props.put(Context.SECURITY_CREDENTIALS, "d8d668942720a6934eaad11fdcdbe423fb79c6f17c92caf85bb99d3431c3d818");
-			props.put(Context.PROVIDER_URL,
-					"postgres://hngiujnqgfyzhs:d8d668942720a6934eaad11fdcdbe423fb79c6f17c92caf85bb99d3431c3d818@ec2-54-247-120-169.eu-west-1.compute.amazonaws.com:5432/d7gp1lhugl5fsn");
+			props.put(Context.PROVIDER_URL, "postgres://hngiujnqgfyzhs:d8d668942720a6934eaad11fdcdbe423fb79c6f17c92caf85bb99d3431c3d818@ec2-54-247-120-169.eu-west-1.compute.amazonaws.com:5432/d7gp1lhugl5fsn");
+			//props.put(Context.INITIAL_CONTEXT_FACTORY, "test");
 			InitialContext ic = new InitialContext();
 			// Locatie van de Database
 			DataSource ds = (DataSource) ic.lookup("java:comp/env/jdbc/PostgresDS");
